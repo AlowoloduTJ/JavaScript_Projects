9bb68215e000a9c1a64b20f973e1d5c3bd4d7012
AlowoloduTJ <alowolodutj@gmail.com>
2025-08-27 14:31:11 +0100

    first commit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+let message = "Hello, Taiwo! Let your Light Shine.";
+window.alert(message);
+document.write("<h2>Genesis 1</h2>");
+
+document.write("<p><strong>Gen 1:1</strong> God created the sky and the earth. At first,</p>");
+
+document.write("<p><strong>Gen 1:2</strong> the earth was completely empty. There was nothing on the earth. Darkness covered the ocean, and God&apos;s Spirit moved over the water.</p>");
+
+document.write("<p><strong>Gen 1:3</strong> Then God said, &quot;Let there be light!&quot; And light began to shine.</p>");
+var B = "Taiwo" + "  Alowolodu";
+document.write(B);
+var family = "The Alowolodus", Dad = "Taiwo", Mom = "Odunayo", Son = "Emmanuel", Daughter = "EriOluwa";
+document.write("<p>Daughter</p>")
+// This whole line is an arithemetic expression:
+//   (5 + 3) * 2  evaluates to 16  
+let result = (5.4 + 3) * 2.55;
+document.write(result); 
---
+++ Project1_expressions_alert/JS/variable_assignment.js
@@ -0,0 +1,2 @@
+let message = "Hello, Taiwo! Let your Light Shine.";
+window.alert(message);
